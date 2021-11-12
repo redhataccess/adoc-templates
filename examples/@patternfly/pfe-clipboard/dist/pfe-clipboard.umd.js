e942a86ba64f256690974518bd8f94830309f2aa
Wes Ruvalcaba <wesruv@gmail.com>
2021-11-12 13:25:38 -0600

    Updating copy button component and fixing adoc error

--- examples/@patternfly/pfe-clipboard/dist/pfe-clipboard.umd.js
+++ examples/@patternfly/pfe-clipboard/dist/pfe-clipboard.umd.js
@@ -80,7 +80,7 @@
   };
 
   /*!
-   * PatternFly Elements: PfeClipboard 1.11.2
+   * PatternFly Elements: PfeClipboard 1.12.0
    * @license
    * Copyright 2021 Red Hat, Inc.
    * 
@@ -112,7 +112,7 @@
 
       // Injected at build-time
       get: function get() {
-        return "\n<style>:host{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-flex:0;-webkit-flex:0 0 auto;-ms-flex:0 0 auto;flex:0 0 auto;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;max-width:-webkit-fit-content;max-width:-moz-fit-content;max-width:fit-content;color:#06c!important;color:var(--pfe-clipboard--Color,var(--pfe-broadcasted--link,#06c))!important;cursor:pointer;padding:6px 16px;padding:var(--pfe-clipboard--Padding,6px 16px);font-weight:300;font-weight:var(--pfe-clipboard--FontWeight,var(--pfe-theme--font-weight--light,300));font-size:1rem;font-size:var(--pfe-clipboard--FontSize,var(--pf-global--FontSize--md,1rem))}:host([hidden]){display:none}.pfe-clipboard__icon{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;width:1em;width:var(--pfe-clipboard--icon--Width,var(--pfe-theme--icon-size,1em));height:auto;height:var(--pfe-clipboard--icon--Height,auto);margin:0 .4825rem 0 0;margin:var(--pfe-clipboard--icon--Margin,0 .4825rem 0 0);--pfe-icon--Color:var(--pfe-clipboard--icon--Color, var(--pfe-theme--color--text--muted, #6a6e73))}.pfe-clipboard__icon svg{fill:#6a6e73!important;fill:var(--pfe-clipboard--icon--Color,var(--pfe-theme--color--text--muted,#6a6e73))!important}.pfe-clipboard__text{color:#06c!important;color:var(--pfe-clipboard--Color,var(--pfe-broadcasted--link,#06c))!important}.pfe-clipboard__text--success{color:#3e8635!important;color:var(--pfe-clipboard--text--success--Color,var(--pfe-theme--color--feedback--success,#3e8635))!important}.pfe-clipboard[copied] .pfe-clipboard__text,:host([copied]) .pfe-clipboard__text{display:none!important}.pfe-clipboard:not([copied]) .pfe-clipboard__text--success,:host(:not([copied])) .pfe-clipboard__text--success{display:none!important}.pfe-clipboard__icon>*,::slotted([slot=icon]){width:100%}:host(:not([aria-disabled=true]).focus-within),:host(:not([aria-disabled=true]):focus){--pfe-clipboard--Color:var(--pfe-clipboard--Color--focus, var(--pfe-broadcasted--link--focus, #004080))}:host(:not([aria-disabled=true])) ::slotted(:hover),:host(:not([aria-disabled=true]):hover){--pfe-clipboard--Color:var(--pfe-clipboard--Color--hover, var(--pfe-broadcasted--link--hover, #004080))}@media screen and (-ms-high-contrast:active),screen and (-ms-high-contrast:none){:host{display:inline-block}.pfe-clipboard__icon{display:inline-block;margin-right:0}.pfe-clipboard__text{display:inline-block}.pfe-clipboard__text--success{display:inline-block}} /*# sourceMappingURL=pfe-clipboard.min.css.map */</style>\n<!-- icon slot -->\n" + (!this.noIcon ? "\n    <div class=\"pfe-clipboard__icon\">\n        <slot name=\"icon\" id=\"icon\">\n            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 15.277 16\"><g transform=\"translate(-2.077 -1.807)\"><path class=\"a\" d=\"M15.34,2.879a3.86,3.86,0,0,0-5.339,0L6.347,6.545a3.769,3.769,0,0,0,0,5.339.81.81,0,0,0,1.132,0,.823.823,0,0,0,0-1.145A2.144,2.144,0,0,1,7.5,7.677l3.641-3.654a2.161,2.161,0,1,1,3.049,3.062l-.8.8a.811.811,0,1,0,1.145,1.132l.8-.8a3.769,3.769,0,0,0,0-5.339Z\" transform=\"translate(0.906 0)\"/><path class=\"a\" d=\"M10.482,6.822a.823.823,0,0,0,0,1.145,2.161,2.161,0,0,1,0,3.049L7.343,14.155a2.161,2.161,0,0,1-3.062,0,2.187,2.187,0,0,1,0-3.062l.193-.116a.823.823,0,0,0,0-1.145.811.811,0,0,0-1.132,0l-.193.193a3.86,3.86,0,0,0,0,5.339,3.86,3.86,0,0,0,5.339,0l3.126-3.139A3.731,3.731,0,0,0,12.72,9.562a3.769,3.769,0,0,0-1.094-2.74A.823.823,0,0,0,10.482,6.822Z\" transform=\"translate(0 1.37)\"/></g></svg>\n        </slot>\n    </div>\n" : "") + "\n<div class=\"pfe-clipboard__text\">\n    <slot name=\"text\" id=\"text\">Copy URL</slot>\n</div>\n<div class=\"pfe-clipboard__text--success\" role=\"alert\">\n    <slot name=\"text--success\" id=\"text--success\">Copied</slot>\n</div>";
+        return "\n<style>:host{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-flex:0;-webkit-flex:0 0 auto;-ms-flex:0 0 auto;flex:0 0 auto;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;max-width:-webkit-fit-content;max-width:-moz-fit-content;max-width:fit-content;color:#06c!important;color:var(--pfe-clipboard--Color,var(--pfe-broadcasted--link,#06c))!important;cursor:pointer;padding:6px 16px;padding:var(--pfe-clipboard--Padding,6px 16px);font-weight:300;font-weight:var(--pfe-clipboard--FontWeight,var(--pfe-theme--font-weight--light,300));font-size:1rem;font-size:var(--pfe-clipboard--FontSize,var(--pf-global--FontSize--md,1rem))}:host([hidden]){display:none}.pfe-clipboard__icon{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;width:1em;width:var(--pfe-clipboard--icon--Width,var(--pfe-theme--icon-size,1em));height:auto;height:var(--pfe-clipboard--icon--Height,auto);margin:0 .4825rem 0 0;margin:var(--pfe-clipboard--icon--Margin,0 .4825rem 0 0);--pfe-icon--Color:var(--pfe-clipboard--icon--Color, var(--pfe-theme--color--text--muted, #6a6e73))}.pfe-clipboard__icon svg{fill:#6a6e73!important;fill:var(--pfe-clipboard--icon--Color,var(--pfe-theme--color--text--muted,#6a6e73))!important}:host([disabled]) .pfe-clipboard__icon{opacity:.4}.pfe-clipboard__text{color:#06c!important;color:var(--pfe-clipboard--Color,var(--pfe-broadcasted--link,#06c))!important}:host([disabled]) .pfe-clipboard__text{color:#6a6e73!important;color:var(--pfe-clipboard--Color--disabled,var(--pfe-theme--color--ui-disabled--text,#6a6e73))!important}.pfe-clipboard__text--success{color:#3e8635!important;color:var(--pfe-clipboard--text--success--Color,var(--pfe-theme--color--feedback--success,#3e8635))!important}:host(:focus:not([disabled])) .pfe-clipboard__text,:host(:hover:not([disabled])) .pfe-clipboard__text{color:#004080!important;color:var(--pfe-clipboard--Color--hover,var(--pfe-broadcasted--link--hover,#004080))!important}:host(:focus:not([disabled])) .pfe-clipboard__icon,:host(:hover:not([disabled])) .pfe-clipboard__icon{--pfe-icon--Color:var(--pfe-clipboard--icon--Color--hover, var(--pfe-theme--color--ui-base--hover, #151515))}:host(:focus:not([disabled])) .pfe-clipboard__icon svg,:host(:hover:not([disabled])) .pfe-clipboard__icon svg{fill:#151515!important;fill:var(--pfe-clipboard--icon--Color--hover,var(--pfe-theme--color--ui-base--hover,#151515))!important}.pfe-clipboard[copied] .pfe-clipboard__text,:host([copied]) .pfe-clipboard__text{display:none!important}.pfe-clipboard:not([copied]) .pfe-clipboard__text--success,:host(:not([copied])) .pfe-clipboard__text--success{display:none!important}.pfe-clipboard__icon>*,::slotted([slot=icon]){width:100%}#icon--url{display:none}:host([copy-from=url]) #icon--url{display:block}:host([copy-from=url]) #icon--copy{display:none}:host([on=dark]){--pfe-clipboard--icon--Color:var(--pfe-clipboard--icon--Color--dark, var(--pfe-theme--color--text--muted--on-dark, #d2d2d2));--pfe-clipboard--icon--Color--hover:var(--pfe-clipboard--icon--Color--hover--dark, var(--pfe-theme--color--text--on-dark, #fff))}:host([on=saturated]){--pfe-clipboard--icon--Color:var(--pfe-clipboard--icon--Color--saturated, var(--pfe-theme--color--text--muted--on-saturated, #d2d2d2));--pfe-clipboard--icon--Color--hover:var(--pfe-clipboard--icon--Color--hover--saturated, var(--pfe-theme--color--text--on-saturated, #fff))}@media screen and (-ms-high-contrast:active),screen and (-ms-high-contrast:none){:host{display:inline-block}.pfe-clipboard__icon{display:inline-block;margin-right:0}.pfe-clipboard__text{display:inline-block}.pfe-clipboard__text--success{display:inline-block}} /*# sourceMappingURL=pfe-clipboard.min.css.map */</style>\n<!-- icon slot -->\n" + (!this.noIcon ? "\n    <div class=\"pfe-clipboard__icon\">\n      <slot name=\"icon\" id=\"icon\">\n        <svg id=\"icon--url\" xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 15.277 16\"><g transform=\"translate(-2.077 -1.807)\"><path class=\"a\" d=\"M15.34,2.879a3.86,3.86,0,0,0-5.339,0L6.347,6.545a3.769,3.769,0,0,0,0,5.339.81.81,0,0,0,1.132,0,.823.823,0,0,0,0-1.145A2.144,2.144,0,0,1,7.5,7.677l3.641-3.654a2.161,2.161,0,1,1,3.049,3.062l-.8.8a.811.811,0,1,0,1.145,1.132l.8-.8a3.769,3.769,0,0,0,0-5.339Z\" transform=\"translate(0.906 0)\"/><path class=\"a\" d=\"M10.482,6.822a.823.823,0,0,0,0,1.145,2.161,2.161,0,0,1,0,3.049L7.343,14.155a2.161,2.161,0,0,1-3.062,0,2.187,2.187,0,0,1,0-3.062l.193-.116a.823.823,0,0,0,0-1.145.811.811,0,0,0-1.132,0l-.193.193a3.86,3.86,0,0,0,0,5.339,3.86,3.86,0,0,0,5.339,0l3.126-3.139A3.731,3.731,0,0,0,12.72,9.562a3.769,3.769,0,0,0-1.094-2.74A.823.823,0,0,0,10.482,6.822Z\" transform=\"translate(0 1.37)\"/></g></svg>\n        <svg id=\"icon--copy\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"32\" height=\"32\" viewBox=\"0 0 32 32\"><g></g><path d=\"M30.286 6.857q0.714 0 1.214 0.5t0.5 1.214v21.714q0 0.714-0.5 1.214t-1.214 0.5h-17.143q-0.714 0-1.214-0.5t-0.5-1.214v-5.143h-9.714q-0.714 0-1.214-0.5t-0.5-1.214v-12q0-0.714 0.357-1.571t0.857-1.357l7.286-7.286q0.5-0.5 1.357-0.857t1.571-0.357h7.429q0.714 0 1.214 0.5t0.5 1.214v5.857q1.214-0.714 2.286-0.714h7.429zM20.571 10.661l-5.339 5.339h5.339v-5.339zM9.143 3.804l-5.339 5.339h5.339v-5.339zM12.643 15.357l5.643-5.643v-7.429h-6.857v7.429q0 0.714-0.5 1.214t-1.214 0.5h-7.429v11.429h9.143v-4.571q0-0.714 0.357-1.571t0.857-1.357zM29.714 29.714v-20.571h-6.857v7.429q0 0.714-0.5 1.214t-1.214 0.5h-7.429v11.429h16z\"/></svg>\n      </slot>\n    </div>\n" : "") + "\n<div class=\"pfe-clipboard__text\">\n    <slot name=\"text\" id=\"text\">Copy</slot>\n</div>\n<div class=\"pfe-clipboard__text--success\" role=\"alert\">\n    <slot name=\"text--success\" id=\"text--success\">Copied</slot>\n</div>";
       }
     }, {
       key: "templateUrl",
@@ -124,13 +124,29 @@
       get: function get() {
         return "pfe-clipboard.scss";
       }
+    }, {
+      key: "contentToCopy",
+      get: function get() {
+        return this._contentToCopy;
+      }
+
+      /**
+       * Adding Getter/Setter for
+       */
+      ,
+      set: function set(contentToCopy) {
+        if (contentToCopy) {
+          this.removeAttribute("disabled");
+          this._contentToCopy = contentToCopy;
+        }
+      }
     }], [{
       key: "version",
 
 
       // Injected at build-time
       get: function get() {
-        return "1.11.2";
+        return "1.12.0";
       }
     }, {
       key: "tag",
@@ -149,7 +165,8 @@
       key: "events",
       get: function get() {
         return {
-          copied: this.tag + ":copied"
+          copied: this.tag + ":copied",
+          connected: this.tag + ":connected"
         };
       }
 
@@ -181,6 +198,11 @@
           tabindex: {
             type: Number,
             default: 0
+          },
+          copyFrom: {
+            type: String,
+            default: "url",
+            observer: "_checkForCopyTarget"
           }
         };
       }
@@ -214,7 +236,14 @@
 
     function PfeClipboard() {
       classCallCheck(this, PfeClipboard);
-      return possibleConstructorReturn(this, (PfeClipboard.__proto__ || Object.getPrototypeOf(PfeClipboard)).call(this, PfeClipboard, { type: PfeClipboard.PfeType }));
+
+      var _this = possibleConstructorReturn(this, (PfeClipboard.__proto__ || Object.getPrototypeOf(PfeClipboard)).call(this, PfeClipboard, { type: PfeClipboard.PfeType }));
+
+      _this._contentToCopy = null;
+
+      _this._checkForCopyTarget = _this._checkForCopyTarget.bind(_this);
+      _this.copyURLToClipboard = _this.copyURLToClipboard.bind(_this);
+      return _this;
     }
 
     createClass(PfeClipboard, [{
@@ -227,13 +256,29 @@
         // the copy functionality
         this.addEventListener("click", this._clickHandler.bind(this));
         this.addEventListener("keydown", this._keydownHandler.bind(this));
+
+        // Make sure the thing we might copy exists
+        this._checkForCopyTarget();
+
+        // Emit event when this component has connected in case copyContent needs to be set
+        this.emitEvent(PfeClipboard.events.connected, {
+          detail: {
+            component: this
+          }
+        });
+
+        // This prevents a regression, default text used to be "Copy URL".
+        // Now that component can copy _anything_ that's not ideal default text
+        if (this.copyFrom === "url" && !this.hasSlot("text")) {
+          this.shadowRoot.getElementById("text").innerText = "Copy URL";
+        }
       }
     }, {
       key: "disconnectedCallback",
       value: function disconnectedCallback() {
+        // Clean up after ourselves
         this.removeEventListener("click", this._clickHandler.bind(this));
         this.removeEventListener("keydown", this._keydownHandler.bind(this));
-        this.shadowRoot.removeEventListener("slotchange", this._slotchangeHandler.bind(this));
         get(PfeClipboard.prototype.__proto__ || Object.getPrototypeOf(PfeClipboard.prototype), "disconnectedCallback", this).call(this);
       }
     }, {
@@ -244,19 +289,86 @@
           this.render();
         }
       }
+
+      /**
+       * Checks to make sure the thing we may copy exists
+       */
+
+    }, {
+      key: "_checkForCopyTarget",
+      value: function _checkForCopyTarget() {
+        if (this.copyFrom === "property") {
+          if (!this._contentToCopy) {
+            this.setAttribute("disabled", "");
+          } else if (this.hasAttribute("disabled")) {
+            this.removeAttribute("disabled");
+          }
+        }
+        // If target is set to anything else, we're not doing checks for it
+        else if (this.copyFrom.length) {
+            this.removeAttribute("disabled");
+          }
+      }
+
+      /**
+       * Event handler for any activation of the copy button
+       */
+
     }, {
       key: "_clickHandler",
-      value: function _clickHandler(event) {
+      value: function _clickHandler() {
         var _this2 = this;
 
+        var text = void 0;
+        switch (this.copyFrom) {
+          // Copy current URL
+          case "url":
+            text = window.location.href;
+            break;
+          // Copy whatever is in this.contentToCopy
+          case "property":
+            if (this._contentToCopy) {
+              text = this._contentToCopy;
+            } else {
+              this.setAttribute("disabled", "");
+              this.error("Set to copy property, but this.contentToCopy is not set");
+              return;
+            }
+            break;
+          // Assume what's in the target property is a selector and copy the text from the element
+          default:
+            var targetElement = document.querySelector(this.copyFrom);
+            if (targetElement && targetElement.tagName) {
+              // What needs to be copied changes for some types of elements
+              switch (targetElement.tagName.toLowerCase()) {
+                // Copy the value of form fields
+                case "input":
+                  text = targetElement.value;
+                  break;
+                // Copy the text of our element
+                default:
+                  text = targetElement.innerText;
+                  break;
+              }
+            }
+            break;
+        }
+
+        if (!text || typeof text === "string" && !text.length) {
+          this.error("Couldn't find text to copy.");
+          this.setAttribute("disabled", "");
+          return;
+        }
+
         // Execute the copy to clipboard functionality
-        this.copyURLToClipboard().then(function (url) {
+        this.copyTextToClipboard(text).then(function (copiedText) {
           // Emit event that lets others know the user has "copied"
           // the url. We are also going to include the url that was
           // copied.
           _this2.emitEvent(PfeClipboard.events.copied, {
             detail: {
-              url: url
+              url: copiedText, // @todo deprecate
+              copiedText: copiedText
             }
           });
           // Toggle the copied state. Use the this._formattedCopiedTimeout function
@@ -267,6 +379,7 @@
           }, _this2._formattedCopiedTimeout());
         }).catch(function (error) {
           _this2.warn(error);
+          _this2._checkForCopyTarget();
         });
       }
 
@@ -307,7 +420,7 @@
       }
 
       /**
-       * Copy url to the user's system clipboard
+       * Copy arbitrary text to system clipboard
        *
        * If available, it will use the new Navigator API to access the system clipboard
        * https://developer.mozilla.org/en-US/docs/Web/API/Navigator/clipboard
@@ -315,31 +428,47 @@
        * If unavailable, it will use the legacy execCommand("copy")
        * https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
        * @async
+       * @param {string} text Text to be copied
+       * @return {Promise<string>} url
+       */
+
+    }, {
+      key: "copyTextToClipboard",
+      value: function copyTextToClipboard(text) {
+        if (!text) this.error("Copy function called, but no text was given to copy.");
+        return new Promise(function (resolve, reject) {
+          // If the Clipboard API is available then use that
+          if (navigator.clipboard) {
+            navigator.clipboard.writeText(text).then(resolve(text));
+          }
+          // If execCommand("copy") exists then use that method
+          else if (document.queryCommandEnabled("copy")) {
+              var dummy = document.createElement("input");
+              document.body.appendChild(dummy);
+              dummy.value = text;
+              dummy.select();
+              document.execCommand("copy");
+              document.body.removeChild(dummy);
+              resolve(text);
+            } else {
+              reject(new Error("Current browser does not support copying to the clipboard."));
+            }
+        });
+      }
+
+      /**
+       * Copy url to the user's system clipboard
+       *
+       * @async
+       * @deprecated This will be removed in version 2.0
        * @return {Promise<string>} url
        */
 
     }, {
       key: "copyURLToClipboard",
       value: function copyURLToClipboard() {
-        return new Promise(function (resolve, reject) {
-          var url = window.location.href;
-          // If the Clipboard API is available then use that
-          if (navigator.clipboard) {
-            navigator.clipboard.writeText(url).then(resolve(url));
-          }
-          // If execCommand("copy") exists then use that method
-          else if (document.queryCommandEnabled("copy")) {
-              var dummy = document.createElement("input");
-              document.body.appendChild(dummy);
-              dummy.value = url;
-              dummy.select();
-              document.execCommand("copy");
-              document.body.removeChild(dummy);
-              resolve(url);
-            } else {
-              reject(new Error("Your browser does not support copying to the clipboard."));
-            }
-        });
+        var url = window.location.href;
+        return this.copyTextToClipboard(url);
       }
     }]);
     return PfeClipboard;
