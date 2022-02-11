2f3d08a7b6dbadff161c17a2fa6b6eef83504778
Wes Ruvalcaba <wesruv@gmail.com>
2022-02-11 11:57:43 -0600

    Mostly functioning Scss refactor

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,25 @@ const
   {
     parallel,
     series,
-    // src,
-    // dest,
+    src,
+    dest,
     task,
     watch,
   } = require('gulp'),
+  gulpIf = require('gulp-if'),
+  sourceMaps = require('gulp-sourcemaps'),
+  sass = require('gulp-sass')(require('sass')),
+  sassGlobbing = require('gulp-sass-globbing'),
+  postCss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssNano = require('cssnano'),
+  pxToRem = require('postcss-pxtorem'),
+  stylelint = require('gulp-stylelint'),
   shell = require('gulp-shell'),
-  browserSync = require('browser-sync').create();
+  browserSync = require('browser-sync').create();;
+
+const isDev = process.env.NODE_ENV === 'dev';
+
 
 // File locations
 const asciiDocTemplates = 'templates';
@@ -36,6 +48,84 @@ const reloadBrowserSync = (done) => {
   done();
 };
 
+// Will be used to ignore globbed files later
+let globbedScssFiles = Array();
+
+/**
+ * Helper with standard globbing config
+ *
+ * @param {string} folderName Name of folder to glob
+ * @param {string} fileName Path (in scss/) to create a partial in the scss folder, e.g. _layout.scss or core/_mixins.scss
+ */
+ const globScssFolder = (folderName, fileName) => {
+  // No idea why, but array methods don't work... but this does.
+  if (Object.values(globbedScssFiles).indexOf(`scss/${fileName}`) === -1) {
+    globbedScssFiles.push(`scss/${fileName}`);
+  }
+
+  // Only glob partials
+  return src(`scss/${folderName}/**/_*.scss`)
+  .pipe(
+    sassGlobbing(
+      {'path': fileName,},
+      {
+        'useSingleQuotes': true,
+        'signature': '// Generated with gulp-sass-globbing',
+      }
+    )
+  )
+  .pipe(dest('scss'));
+};
+
+// Need an anon function so we can call a function that needs params
+// Making a var so the gulp output will be more informative
+const globBaseScss = () => globScssFolder('20_base', '_base.scss');
+const globComponentsScss = () => globScssFolder('30_components', '_components.scss');
+const globLayoutScss = () => globScssFolder('40_layout', '_layout.scss');
+
+const globScss = parallel(
+  globBaseScss,
+  globComponentsScss,
+  globLayoutScss
+);
+
+/**
+ * Compile an SCSS file to CSS
+ * @param {String} source File path
+ * @param {String} output File path
+ */
+const processScss = (source) =>
+  src(source)
+  // Lint first
+  .pipe(stylelint({
+    reporters: [
+      {
+        formatter: 'string',
+        console: true,
+        failAfterError: false,
+      }
+    ]
+  }))
+  // Start compiling
+  .pipe(gulpIf(isDev, sourceMaps.init()))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(
+    postCss([
+      pxToRem({
+        'propList': ['*',],
+      }),
+      autoprefixer(),
+    ])
+  )
+  // Minify if production build
+  .pipe(gulpIf(!isDev, postCss([cssNano(),])))
+  .pipe(dest('examples/'))
+  .pipe(sourceMaps.write())
+  .pipe(dest('dist/'));
+
+const compileRhDocs = () => processScss('scss/rhdocs.scss');
+const globAndCompileRhDocs = series(globScss, compileRhDocs);
+
 const compileAsciiDoc = (path, env) =>  {
   return shell.task(`asciidoctor -T ${asciiDocTemplates} -a pantheonenv=${env} ${path}`);
 }
@@ -46,7 +136,6 @@ const compileAllAsciiDocs = (env) => series(
   compileAsciiDoc('examples/assembly_access-control-list.adoc', env),
   compileAsciiDoc('examples/demo-assembly.adoc', env),
 );
-
 
 const watchTasks = () => {
   watch(
@@ -64,25 +153,36 @@ const watchTasks = () => {
       reloadBrowserSync
     )
   );
+
+  // Don't watch globbed files to avoid an infinite loop
+  const watchedScss = globbedScssFiles.map(file => `!${file}`);
+  watchedScss.unshift('scss/**/*.scss');
+
+  watch(
+    watchedScss,
+    series(
+      globAndCompileRhDocs,
+      reloadBrowserSync
+    )
+  )
 };
 
 /**
  * Gulp tasks
  */
 // Will use frontend assets from prod
-task('default',
-  compileAllAsciiDocs('localdev')
-);
-
-// Will use local frontend assets
-task('build:dev',
+exports.default = series(
+  globAndCompileRhDocs,
   compileAllAsciiDocs('localdev')
 );
 
 // Starts browsersync, watches project for changes and reloads all browsers
 task('watch',
   series(
-    compileAllAsciiDocs('localdev'),
+    parallel(
+      compileAllAsciiDocs('localdev'),
+      globAndCompileRhDocs
+    ),
     parallel(
       startBrowserSync,
       watchTasks
